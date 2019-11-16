eaa0f476fd026581b922007724fdde4978ff70e9
jhart3000 <j.hart4000@gmail.com>
2019-11-16 17:11:07 +0000

    initial commit

---
+++ src/test/java/com/categorise/transactions/fractal/FractalAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.categorise.transactions.fractal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FractalAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
